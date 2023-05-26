96728835564f82746b22ac4aa4b6875d687b06e0
USMortality.com <USMortality@protonmail.com>
2023-05-26 16:09:04 -0700

    add us national 5y data

---
+++ tests/monthly/1999_2020_national.spec.js
@@ -0,0 +1,49 @@
+import { test } from '@playwright/test'
+import { makeSequence, waitUntilLoaded } from '../common.js'
+
+const dl = async (page, ageGroups) => {
+  await page.goto('https://wonder.cdc.gov/mcd-icd10.html')
+  await page.getByRole('button', { name: 'I Agree' }).click()
+
+  await page.locator('#SB_1').selectOption('D77.V1-level2') // group by: month
+
+  await waitUntilLoaded(page)
+  await page.locator('#RO_ageD77\\.V52').click()
+  await waitUntilLoaded(page)
+  await page.locator('#SD77\\.V52').selectOption(ageGroups) // select ages
+  await waitUntilLoaded(page)
+
+  await page.click('#submit-button1') // submit form
+  await page.waitForSelector('#wonderform > table > tbody > tr > td > div.wonder-content > div > table.response-form')
+  await waitUntilLoaded(page)
+  const downloadPromise = page.waitForEvent('download')
+  await page.getByRole('button', { name: 'Export' }).dblclick()
+  const download = await downloadPromise
+  const ag = Array.isArray(ageGroups)
+    ? `${ageGroups[0]}-${ageGroups[ageGroups.length - 1]}` : ageGroups
+  await download.saveAs(`./out/monthly/us_1999-2020_${ag}.txt`)
+}
+
+test('Download CDC Wonder Data 1999-2020 [5y groups].', async ({ page }) => {
+  await dl(page, makeSequence(0, 4))
+  await dl(page, makeSequence(5, 9))
+  await dl(page, makeSequence(10, 14))
+  await dl(page, makeSequence(15, 19))
+  await dl(page, makeSequence(20, 24))
+  await dl(page, makeSequence(25, 29))
+  await dl(page, makeSequence(30, 34))
+  await dl(page, makeSequence(35, 39))
+  await dl(page, makeSequence(40, 44))
+  await dl(page, makeSequence(45, 49))
+  await dl(page, makeSequence(50, 54))
+  await dl(page, makeSequence(55, 59))
+  await dl(page, makeSequence(60, 64))
+  await dl(page, makeSequence(65, 69))
+  await dl(page, makeSequence(70, 74))
+  await dl(page, makeSequence(75, 79))
+  await dl(page, makeSequence(80, 84))
+  await dl(page, makeSequence(85, 89))
+  await dl(page, makeSequence(90, 94))
+  await dl(page, makeSequence(95, 100))
+  await page.close()
+})
