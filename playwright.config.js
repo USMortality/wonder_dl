fbd9c3519b3d7b3abcde7e8d71711df2a3c3a76f
USMortality.com <USMortality@protonmail.com>
2023-11-09 21:27:15 -0800

    update reporter

--- playwright.config.js
+++ playwright.config.js
@@ -16,13 +16,14 @@ module.exports = defineConfig({
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  timeout: 1000 * 60 * 60, // 1h
+  timeout: 1000 * 60 * 10, // 1h
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  // Concise 'dot' for CI, default 'list' when running locally
+  reporter: 'list',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -30,7 +31,8 @@ module.exports = defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    headless: process.env.HEADLESS ? true : false
+    headless: process.env.HEADLESS ? true : false,
+    video: 'on'
   },
 
   /* Configure projects for major browsers */
