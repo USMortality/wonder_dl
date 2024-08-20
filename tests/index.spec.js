d54eefc584bf6b7d6b53a99a0738ea941352b919
USMortality.com <USMortality@protonmail.com>
2024-08-20 10:57:01 -0700

    update test

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,5 +1,5 @@
 import { test } from '@playwright/test'
-import * as fs from 'fs'
+import * as fs from 'fs/promises'
 import * as Minio from 'minio'
 import { waitUntilLoaded } from './common'
 import { readFileSync } from 'fs'
@@ -7,50 +7,65 @@ import { readFileSync } from 'fs'
 const s3Client = new Minio.Client({
   endPoint: process.env.AWS_DEFAULT_REGION + '.' + process.env.AWS_S3_ENDPOINT,
   accessKey: process.env.AWS_ACCESS_KEY_ID,
-  secretKey: process.env.AWS_SECRET_ACCESS_KEY
+  secretKey: process.env.AWS_SECRET_ACCESS_KEY,
 })
 
 test.use({ deviceScaleFactor: 2, viewport: { width: 700, height: 1200 } })
 
 const saveChart = async (page, type, iso) => {
   const canvas = await page.locator('canvas')
-  const image = await canvas.evaluate(x => x.toDataURL().substring(22))
+  const image = await canvas.evaluate((x) => x.toDataURL().substring(22))
   const file = `${iso}.png`
   const filepath = `./out/${type}/${file}`
-  return new Promise((resolve, reject) => {
-    fs.mkdir(`./out/${type}`, { recursive: true }, (err) => {
-      fs.writeFile(filepath, image, 'base64', (err) => {
-        if (err) console.log(err)
-        s3Client.fPutObject('charts', `mortality/${type}/${file}`, filepath, {}, err => {
-          if (err) return reject(err)
-          resolve()
-        })
-      })
-    })
-  })
+
+  try {
+    await fs.mkdir(`./out/${type}`, { recursive: true })
+    await fs.writeFile(filepath, image, 'base64')
+    console.log(`File saved: ${filepath}`)
+
+    await s3Client.fPutObject(
+      'charts',
+      `mortality/${type}/${file}`,
+      filepath,
+      {}
+    )
+    console.log(`File uploaded to S3: mortality/${type}/${file}`)
+  } catch (err) {
+    console.error(`Error in saveChart for ${iso}:`, err)
+    throw err
+  }
 }
 
 const countries = JSON.parse(readFileSync('./out/countries.json'))
 
-let page
-test.beforeAll(async ({ browser }) => {
-  page = await browser.newPage()
-  await page.evaluate(() => window.disableToast = true)
+test.describe('Save Mortality Charts', () => {
+  countries.cmr.forEach((iso) => {
+    test(`Save 52W Mortality, CMR [${iso}]`, async ({ browser }) => {
+      const page = await browser.newPage()
+      try {
+        const url = `https://www.mortality.watch/explorer/?c=${iso}&t=cmr&ct=weekly_52w_sma&v=2`
+        console.log(`Navigating to: ${url}`)
+        await page.goto(url)
+        await waitUntilLoaded(page)
+        await saveChart(page, 'cmr', iso)
+      } finally {
+        await page.close()
+      }
+    })
+  })
+
+  countries.asmr.forEach((iso) => {
+    test(`Save 52W Mortality, ASMR [${iso}]`, async ({ browser }) => {
+      const page = await browser.newPage()
+      try {
+        const url = `https://www.mortality.watch/explorer/?c=${iso}&t=asmr&ct=weekly_52w_sma&v=2`
+        console.log(`Navigating to: ${url}`)
+        await page.goto(url)
+        await waitUntilLoaded(page)
+        await saveChart(page, 'asmr', iso)
+      } finally {
+        await page.close()
+      }
+    })
+  })
 })
-test.afterAll(async ({ }) => { page.close() })
-
-for (const iso of countries.cmr) {
-  test(`Save 52W Mortality, CMR [${iso}]`, async () => {
-    await page.goto(`https://www.mortality.watch/explorer/?c=${iso}&t=cmr&ct=weekly_52w_sma&v=2`)
-    await waitUntilLoaded(page)
-    await saveChart(page, "cmr", iso)
-  })
-}
-
-for (const iso of countries.asmr) {
-  test(`Save 52W Mortality, ASMR [${iso}]`, async () => {
-    await page.goto(`https://www.mortality.watch/explorer/?c=${iso}&t=asmr&ct=weekly_52w_sma&v=2`)
-    await waitUntilLoaded(page)
-    await saveChart(page, "asmr", iso)
-  })
-}
